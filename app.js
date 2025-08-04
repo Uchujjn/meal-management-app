0f7260b70a66225f96df63ade97a4651f5781980
Uchujjn <67218361+Uchujjn@users.noreply.github.com>
2025-08-04 23:10:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -225,7 +225,7 @@ function updatePFCChart(total) {
       labels: ['Protein', 'Fat', 'Carbs'],
       datasets: [{
         data: [total.protein, total.fat, total.carbs],
-        backgroundColor: ['#2196f3', '#f44336', '#ffeb3b']
+        backgroundColor: ['#fc6759ff', '#f9da5eff', '#cf9458ff']
       }]
     },
     options: { responsive: true }
